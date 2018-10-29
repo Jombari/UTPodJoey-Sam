5482a1e2d2b2a94235fd8c4332b1f602e77d1343
Jombari <JosephTombari@gmail.com>
2018-10-28 23:08:25 -0500

    Update UtPod.cpp

--- UtPod.cpp
+++ UtPod.cpp
@@ -4,3 +4,11 @@
 UtPod::UtPod(void){
   cout << "Object has been created" << endl;
 }
+UtPod::UtPod(int size){
+  if((size>MAX_MEMORY) || (size <= 0)){
+   memsize = MAX_MEMORY;
+  }
+  else{
+    memsize = size;
+  }
+}
