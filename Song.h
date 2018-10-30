35cdfdd2245ea606d6b55fa8b0ac110d53bf1b20
Jombari <JosephTombari@gmail.com>
2018-10-30 11:15:04 -0500

    Update Song.h

--- Song.h
+++ Song.h
@@ -14,16 +14,39 @@ private:
     
     
 public:
+//Default Constructor
+    Song();
+    
+//Overload Constructor
     Song(string songTitle, int songSize, string songArtist);
 
+//Destructor
+    ~Song();
+    
+//Accessor Functions
+    
+    //gets Title of Song
     string getTitle() const;
-
+    
+    //gets Artist Name
     string getArtist() const;
-
+        
+    //Gets the int value of how much memory the song takes up
     int getMemory() const;
+    
+    
+//Modifier Functions
+    
+    //Sets the value of Song Memory size
     void setMemory(int size);
+    
+    //Sets the Song Artist Name
     void setArtist(string artistName);
+    
+    //Sets the Song Title Name
     void setSongTitle(string song);
+    
+    
     bool operator ==(Song const &song);
     bool operator <(Song const &song);
     bool operator >(Song const &song);
